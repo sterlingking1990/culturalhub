de64d1cb18028980f166ab27ce35cfb0dd567336
Rails Udemy Course <izundukingsleyemeka@gmail.com>
2018-11-22 21:38:38 +0100

    update database

--- config/database.php
+++ config/database.php
@@ -1,5 +1,12 @@
 <?php
 
+// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
+
 return [
 
     /*
@@ -41,11 +48,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'db4free.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'culturehubdb1_'),
+            'username' => env('DB_USERNAME', 'culturehubroot1_'),
+            'password' => env('DB_PASSWORD', 'kingsley1990'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
